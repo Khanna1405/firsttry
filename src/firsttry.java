6e15d656569206254b60d06ccd247a800292fcfb
Gourav Khanna <gourav.khanna@profinch.com>
2020-09-28 14:55:13 +0530

    first branch commit

--- src/firsttry.java
+++ src/firsttry.java
@@ -2,5 +2,6 @@ public class firsttry {
     public static void main(String[] args) {
         int a =5;
         System.out.println("My son age is "+a+" years.");
+        System.out.println("Age: "+a);
     }
 }
